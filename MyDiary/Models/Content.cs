c6afd1caae4cfefdd489282f6879093f507af38f
Shinhee Kim <Shinhee.Kim@GeorgianCollege.ca>
2019-09-30 21:17:52 -0400

    Add project files.

---
+++ MyDiary/Models/Content.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDiary.Models
+{
+    public class Content
+    {
+        [Key]
+        public int ContentId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string Writer { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+
+    }
+}
---
+++ MyDiary/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDiary.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+
+        [ForeignKey("Content")]
+        public int ContentId { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string Writer { get; set; }
+
+    }
+}
