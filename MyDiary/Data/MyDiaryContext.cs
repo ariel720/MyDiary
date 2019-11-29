1d66140d4fbfa476f51fa7764ae496af6128ecb1
Shinhee Kim <Shinhee.Kim@GeorgianCollege.ca>
2019-11-29 14:39:32 -0500

    commit

--- MyDiary/Data/MyDiaryContext.cs
+++ MyDiary/Data/MyDiaryContext.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MyDiary.Models;
 
 namespace MyDiary.Models
 {
-    public class MyDiaryContext : DbContext
+    public class MyDiaryContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         public MyDiaryContext (DbContextOptions<MyDiaryContext> options)
             : base(options)
